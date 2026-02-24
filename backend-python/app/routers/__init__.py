99cf6195567af7d9a3f21eec5b74e522211f388d
estiven-piedrahita <duvanestivenpiedrahita@gmail.com>
2026-02-24 13:01:18 -0500

    work done in database that should go to landing

---
+++ backend-python/app/routers/__init__.py
@@ -0,0 +1,3 @@
+from .ia_router import router as ia_router
+
+__all__ = ['ia_router']
---
+++ backend-python/app/services/__init__.py
@@ -0,0 +1,4 @@
+from .groq_service import generate_plan_with_groq
+from .prompt_builder import build_personalized_prompt
+
+__all__ = ['generate_plan_with_groq', 'build_personalized_prompt']
